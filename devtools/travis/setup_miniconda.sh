186a6a0cfc4d0e54236c353dcf41918cab403750
Moritz Hoffmann <clonker@gmail.com>
2017-02-24 11:26:17 +0100

    fix stuff

--- devtools/travis/setup_miniconda.sh
+++ devtools/travis/setup_miniconda.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-TRUNC_CONDA_VER=${CONDA_PY:0:1}
 echo "got truncated conda version:"
 echo "--------"
-echo ${TRUNC_CONDA_VER}
+echo ${PY_VER}
 echo "--------"
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then OS=Linux-x86_64; else OS=MacOSX-x86_64; fi
-wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda${TRUNC_CONDA_VER}-latest-$OS.sh
+wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda${PY_VER}-latest-$OS.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda install -q conda-build
-echo "creating conda env with python=${TRUNC_CONDA_VER}"
-conda create -q -n test-environment python=${TRUNC_CONDA_VER}
+echo "creating conda env with python=${PY_VER}"
+conda create -q -n test-environment python=${PY_VER}
 echo "available conda envs:"
 conda info --envs
