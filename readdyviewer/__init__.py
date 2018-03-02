a5d6f01b9ea7a7024cd54202498067fa9031ec64
Moritz Hoffmann <clonker@gmail.com>
2018-03-02 10:51:09 +0100

    f

--- readdyviewer/__init__.py
+++ readdyviewer/__init__.py
@@ -1,6 +1,6 @@
-from . import readdyviewer
+from .readdyviewer_binding import Color, Configuration, TrajectoryEntry, set_resource_dir, watch_npy
 import os as _os
 
 _ROOT = _os.path.abspath(_os.path.dirname(__file__))
 
-readdyviewer.set_resource_dir(_os.path.join(_ROOT, 'resources'))
+set_resource_dir(_os.path.join(_ROOT, 'resources'))
